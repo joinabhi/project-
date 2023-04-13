4a6e8daab5e7fa762e31c784734da3542f76afe4
joinabhi <abhi.gpt96@gmail.com>
2023-04-13 10:51:49 +0530

    text changes

--- dom.js
+++ dom.js
@@ -1,6 +1,9 @@
 var items=document.getElementsByClassName('list-group-item')
 console.log(items)
 items[2].style.backgroundColor="green"
+items[2].textContent="Hello"
 for(var i=0;i<items.length;i++){
     items[i].style.fontWeight="bold"
 }
+var li=document.getElementsByTagName('li')
+li[1].textContent="hello"
