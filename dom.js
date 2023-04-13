5c4e33f79b9b019fd88773bc6b647182457b6489
joinabhi <abhi.gpt96@gmail.com>
2023-04-13 23:26:37 +0530

    add new file

--- dom.js
+++ dom.js
@@ -1,7 +1,7 @@
-var secondItems=document.querySelector('.list-group-item:nth-child(2)')
-secondItems.setAttribute("style", "color:rgb(50,150,200); background-color:green");	
-var thirdItems=document.querySelector('.list-group-item:nth-child(3)')
-thirdItems.style.display = 'none'
+var itemList=document.querySelector('#items')
+console.log(itemList)
+itemList.parentNode.style.backgroundColor='#f4f4f4';
+console.log(itemList.parentNode.parentNode)
  
 
 
