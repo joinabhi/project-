a61e471c593bb29ac19e726443fbda2b51fa9ef2
joinabhi <abhi.gpt96@gmail.com>
2023-04-26 13:23:59 +0530

    Some changes in this specific file

---
+++ index10.js
@@ -0,0 +1,17 @@
+var obj = {
+
+    val: 100
+    
+    }
+    
+    function fun(a, b , c){
+    
+    console.log(a)
+    
+    console.log(b)
+    
+    console.log(c)
+    
+    }
+    
+    // fun.call(obj, [3, 4, 5])
